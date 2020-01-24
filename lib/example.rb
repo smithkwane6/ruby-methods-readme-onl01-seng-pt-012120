cdad44100181929f418e4d815585a585d8b79730
Kwane Smith <smithkwane6@gmail.com>
2020-01-24 00:55:52 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,5 @@
 # Use this space to code along with the readme
-
+def say_hello_ten_times
 phrase = "Hello World!"
 puts phrase
 puts phrase
@@ -11,3 +11,9 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+end
+say_hello_ten_times
+
+def a_sample_method
+  puts "I am the method body"
+end
